815c9944fa17798374f8aaacdb3323705b2492c5
Awiddy Munaya Rajanadoli <awiddy.munaya@10.5.50.211>
2026-09-19 19:42:54 +0700

    Perbaikan final data migration

--- main/migrations/0005_auto_20260919_1240.py
+++ main/migrations/0005_auto_20260919_1240.py
@@ -2,17 +2,15 @@ from django.db import migrations
 from django.core.management import call_command
 
 def load_my_initial_data(apps, schema_editor):
-    # Memuat file initial_data.json ke dalam database saat migrasi berjalan
     call_command("loaddata", "initial_data.json")
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        # BIARKAN BARIS INI APA ADANYA SESUAI BAWAAN DJANGO!
-        # (Biasanya berisi: ('main', '0004_education_description...'))
+        # Ini adalah nama file migrasi ke-4 milikmu yang tepat dan valid
+        ('main', '0004_education_description_alter_education_end_year_and_more'),
     ]
 
     operations = [
-        # Tambahkan baris ini ke dalam operations
         migrations.RunPython(load_my_initial_data),
     ]
